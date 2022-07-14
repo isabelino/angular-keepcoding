0f60e71108c5b6df5eccb9d5eef3c35768b219d9
rolando <isa.klopez@hotmail.com>
2022-07-14 13:06:37 +0200

    primer comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AcumuladorModule } from './acumulador/acumulador.module';
 import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    AcumuladorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
